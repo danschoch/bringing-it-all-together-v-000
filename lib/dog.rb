6aca62af8acedad7752738805bc6d7472562b0f3
Daniel Schoch <d.schoch90@gmail.com>
2018-10-04 18:20:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,11 @@ class Dog
   end
 
   def self.find_by_name (name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?;
+    SQL
 
+    DB[:conn].execute (sql)
   end
 
   def self.find_by_id(id)
