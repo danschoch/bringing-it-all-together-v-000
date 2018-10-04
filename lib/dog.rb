00f81eaa659cbd587678febd4356da906bff4ea6
Daniel Schoch <d.schoch90@gmail.com>
2018-10-04 19:04:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,8 +26,10 @@ class Dog
   end
 
   def self.new_from_db (row)
-    new_dog = self.new (id: row[0], name: row[1], breed: row[2])
-    new_dog
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name (name)
